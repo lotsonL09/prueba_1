b6a016714068bdbcf32c34582d1b44977b9efa1e
William Valencia <willimaca09ac@hotmail.com>
2023-01-02 21:39:01 -0500

    Reto 01-2023

---
+++ papelito.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from datetime import time
+
+time_2=time(10,22,14)
+
+time_now=datetime.time(datetime.now())
+print(time_now)
+print(type(time_now))
+
+print(time_2)
+print(type(time_2))
